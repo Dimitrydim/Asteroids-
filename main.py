abb00d0642c053cac6800715786035a1f2c2f1f3
Dimitrydim <dimitrydim@hotmail.com>
2025-04-22 14:02:00 -0600

    Adding screen to asteroids

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from constants import *
 import pygame
 def main():
+	pygame.init
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		pygame.Surface.fill(screen, pygame.Color("black"))
+		pygame.display.flip()
 if __name__ == "__main__":
 	main()
